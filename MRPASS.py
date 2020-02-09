3f8b35aad31f1cd752113b7e7733ea9294f7c043
btaruns22 <31902746+btaruns22@users.noreply.github.com>
2020-02-09 18:42:39 -0500

    Initial commit

---
+++ MRPASS.py
@@ -0,0 +1,18 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-", ]
+
+print(board[0] + " " + board[1] + " " + board[2] + " \n" +
+      board[3] + " " + board[4] + " " + board[5] + " \n" +
+      board[6] + " " + board[7] + " " + board[8] + " \n")
+
+user = int(input("Enter position: "))
+c = 1  # control ultimate while loop
+i = 1  # control while loop turns
+# c = 0 this will stop the loop
+turn = 0
+
+
+
+
+print("done")
