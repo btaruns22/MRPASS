b3ba2839d0717cd7a79226ddc40a84bb4689ac5c
btaruns22 <31902746+btaruns22@users.noreply.github.com>
2020-02-12 19:15:31 -0500

    fdsa

--- MRPASS.py
+++ MRPASS.py
@@ -12,4 +12,5 @@ window.geometry("800x500")
 ProgName = tkinter.Label(window, font=('MR.PASS', 30, 'bold'), text="PassManager", fg="blue")
 ProgName.grid(row=1, column=5, padx=330, pady=200)
 
+
 window.mainloop()
---
+++ Stuff.py
@@ -0,0 +1,54 @@
+# for i in range (1,11):
+#   print(i, end= " ")
+'''
+for i in range(2,7):
+    for x in range(1,i):
+        print(x, end =" ")
+
+    print("")
+
+'''
+'''
+for j in range (1,6):
+    for i in range (j):
+        print("a", end = " ")
+    for k in range (5-j):
+        print ("b", end = " ")
+    print ()
+'''
+'''
+max = 0
+for i in range (10):
+     n = int(input("Enter a number: "))
+     if (n >= 0 and n > max):
+          max = n
+
+print (max)
+'''
+'''
+for i in range(3):
+   for j in range(3-i):
+       print("*", end=" ")
+   print("")
+'''
+'''
+print("FOURTH")
+for i in range(1,11):
+    for j in range(i):
+        print(" ", end = " ")
+    for k in range (11-i):
+        print ("*", end =" ")
+    print("")
+'''
+'''
+for i in range(11):
+    for j in range(i):
+        print("o", end=" ")
+    for k in range(11 - i):
+        print("*", end=" ")
+    print()
+'''
+
+
+for i in range (11):
+    print (i, end = " ")
